732fa8d3783ba5e8e3270ff7a233d68c681ed782
Jothsna Sri Kathyayani <94035566+ch-jsk@users.noreply.github.com>
2024-08-23 08:57:31 +0530

    java p

---
+++ operators.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class oprators {
+    public static void main(String args[]) {
+       
+     arithmetic oprator------------>>>>binary oprator  
+        int a = 10;
+        int b = 5;
+        System.out.println("add = " + (a+b));
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+
+    
+        // unary operator:::
+        // pre_increment->>>>  ++a
+        // post increment->>>  a++
+
+        int a = 10;
+        //int b = ++a;
+        int b = a++;
+        // int b = --a;
+        // int b = a--;
+
+        System.out.println(b);
+        System.out.println(a);
+
+
+        // RELATIONAL OPRATIORS----------
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+        // System.out.println(a<b);
+        // System.out.println(a>b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+
+
+        //LOGICAL OPRATORS
+        // System.out.println( (3<2) && (5>0) );
+        // System.out.println( (3<2) || (5>0) );
+        // System.out.println( !(3<2));
+
+
+        //ASSIGNMENT OPERATOR
+        //  int a = 10;
+        // a = a+10;
+        // a += 10;
+        // int b = 10;
+        // b -= 5;
+
+        // a *= 5;
+        // a /= 5;
+
+        // a %= 5;
+        // System.out.println(a);
+        
+
+
+
+    }
+}
